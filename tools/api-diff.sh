f3a464ac1827fd272d28be2e307e1b940ace213e
Tim Cuthbertson <tim@gfxmonk.net>
2014-02-06 09:32:17 +1100

    add api-diff tool

---
+++ tools/api-diff.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -eu
+set -o pipefail
+
+if [ "$#" -lt 2 ]; then
+	echo "Usage: $0 old-commit new-commit" >&2
+	exit 1
+fi
+
+git rev-parse "$1" "$2"
+jspp="0install run http://gfxmonk.net/dist/0install/python-js-beautify.xml -i -"
+getApi () {
+	git rev-parse "$1"
+	git show "$1:modules/sjs-lib-index.json" | $jspp
+}
+diff --side-by-side <(getApi "$1") <(getApi "$2") | less
